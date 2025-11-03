59a9a81845d9d3a9c13de1926c7cf12d40c50989
a1masssss <almas.issakov.t@gmail.com>
2025-11-03 19:59:52 +0500

    Remove unused React import from TimelineSection

--- frontend/src/pages/Timeline/TimelineSection.tsx
+++ frontend/src/pages/Timeline/TimelineSection.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Timeline } from "@/components/ui/timeline";
 import { Award, Briefcase, Lightbulb, TrendingUp, Users, DollarSign, Target, Rocket } from "lucide-react";
 
